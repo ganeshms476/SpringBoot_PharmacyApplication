1645cd6768a3177c067aa2d88f1d06e535b6c3d4
ganeshms476 <ganeshms476@gmail.com>
2023-02-23 10:31:42 +0530

    pharmacy app

---
+++ src/test/java/com/ty/SpringBootPharmacyApplication/SpringBootPharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ty.SpringBootPharmacyApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPharmacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
