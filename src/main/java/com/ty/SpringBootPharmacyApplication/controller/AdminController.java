d11f3073e0af2bd3f1262bd4f6f62c9472e1eb80
ganeshms476 <ganeshms476@gmail.com>
2023-02-27 12:40:28 +0530

    admin controller

---
+++ src/main/java/com/ty/SpringBootPharmacyApplication/controller/AdminController.java
@@ -0,0 +1,45 @@
+package com.ty.SpringBootPharmacyApplication.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ty.SpringBootPharmacyApplication.dto.Admin;
+import com.ty.SpringBootPharmacyApplication.service.AdminService;
+import com.ty.SpringBootPharmacyApplication.util.ResponseStructure;
+
+@RestController
+public class AdminController {
+
+	@Autowired
+	private AdminService service;
+
+	@PostMapping("/admin")
+	public ResponseEntity<ResponseStructure<Admin>> saveAdmin(@RequestBody Admin admin) {
+		return service.saveAdmin(admin);
+	}
+
+	@PutMapping("/admin/{id}")
+	public ResponseEntity<ResponseStructure<Admin>> updateAdmin(@PathVariable int id, @RequestBody Admin admin) {
+		return service.updateAdmin(id, admin);
+	}
+
+	@DeleteMapping("/admin")
+	public ResponseEntity<ResponseStructure<Admin>> deleteAdmin(@RequestParam int id) {
+		return service.deleteAdmin(id);
+	}
+
+	@GetMapping("/admin")
+	public ResponseEntity<ResponseStructure<Admin>> getAdminByid(@PathVariable int id, @RequestBody Admin admin) {
+		return service.updateAdmin(id, admin);
+	}
+
+}
