8f9451f7d0a26423c1595af50f2a942382b3e2dd
ganeshms476 <ganeshms476@gmail.com>
2023-02-27 14:20:09 +0530

    admin

--- src/main/java/com/ty/SpringBootPharmacyApplication/dao/AdminDAO.java
+++ src/main/java/com/ty/SpringBootPharmacyApplication/dao/AdminDAO.java
@@ -8,11 +8,42 @@ import com.ty.SpringBootPharmacyApplication.repository.AdminRepository;
 
 @Repository
 public class AdminDAO {
-	
+
 	@Autowired
 	AdminRepository repo;
-	
+
 	public Admin saveAdmin(Admin admin) {
-		
+		return repo.save(admin);
+	}
+
+	public Admin loginAdmin(String email) {
+		return repo.loginUsingEmail(email);
+	}
+
+	public Admin updateAdmin(int id, Admin admin) {
+		if (repo.findById(id).isPresent()) {
+			admin.setId(id);
+			return repo.save(admin);
+		} else {
+			return null;
+		}
+	}
+
+	public Admin deleteAdmin(int id) {
+		if (repo.findById(id).isPresent()) {
+			Admin admin = repo.findById(id).get();
+			repo.deleteById(id);
+			return admin;
+		} else {
+			return null;
+		}
+	}
+
+	public Admin getAdminById(int id) {
+		if (repo.findById(id).isPresent()) {
+			return repo.findById(id).get();
+		} else {
+			return null;
+		}
 	}
 }
--- src/main/java/com/ty/SpringBootPharmacyApplication/repository/AdminRepository.java
+++ src/main/java/com/ty/SpringBootPharmacyApplication/repository/AdminRepository.java
@@ -1,10 +1,12 @@
 package com.ty.SpringBootPharmacyApplication.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ty.SpringBootPharmacyApplication.dto.Admin;
 
 
 public interface AdminRepository extends JpaRepository<Admin, Integer>{
-
+	@Query("Select e from Admin e where e.email=?1")
+	public Admin loginUsingEmail(String email);
 }
