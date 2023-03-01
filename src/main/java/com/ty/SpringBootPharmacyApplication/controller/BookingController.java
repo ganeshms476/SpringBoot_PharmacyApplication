183b4583cef7479f542500e60e81f2230a91a5ca
unknown <shobhithaas18@gmail.com>
2023-03-01 11:51:21 +0530

    bookin service

---
+++ src/main/java/com/ty/SpringBootPharmacyApplication/controller/BookingController.java
@@ -0,0 +1,30 @@
+package com.ty.SpringBootPharmacyApplication.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ty.SpringBootPharmacyApplication.dto.Address;
+import com.ty.SpringBootPharmacyApplication.dto.Booking;
+import com.ty.SpringBootPharmacyApplication.service.BookingService;
+import com.ty.SpringBootPharmacyApplication.util.ResponseStructure;
+
+@RestController
+public class BookingController {
+	@Autowired
+	private BookingService service;
+	
+	 @PostMapping("/booking")
+	 public ResponseEntity<ResponseStructure<Booking>> saveBooking(@RequestParam int customer_id, @RequestBody Booking booking, @RequestParam int medicine_id){
+		 return service.saveBooking(customer_id, booking, medicine_id);
+	 }
+	 
+	 
+
+}
